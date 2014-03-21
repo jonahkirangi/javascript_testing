efddc84e9271adf3b1f798d67768bd0dfe072b84
Jonah Kirangi <jonahkirangi@gmail.com>
2014-03-20 23:39:31 -0700

    Add userlist

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' });
+};
+
+exports.about = function(req, res){
+  res.render('about', { title: 'About' });
+};
+
+exports.userlist = function(db) {
+    return function(req, res) {
+        var collection = db.get('usercollection');
+        collection.find({},{},function(e,docs){
+            res.render('userlist', {
+                "userlist" : docs
+            });
+        });
+    };
+};
